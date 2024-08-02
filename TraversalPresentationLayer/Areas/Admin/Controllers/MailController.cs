098a543c6155765c4c72fe51bf5a1de340be5072
Burak Arslan <burakarslan0110@gmail.com>
2024-08-02 23:51:03 +0300

    Adding Mail Send System
    Adding Mail Send System

---
+++ TraversalPresentationLayer/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalPresentationLayer.Models;
+
+namespace TraversalPresentationLayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("MyTraversalProject", "mytraversalproject@gmail.com");
+            
+            mimeMessage.From.Add(mailboxAddressFrom);
+            
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodybuilder = new BodyBuilder();
+            bodybuilder.TextBody = mailRequest.Body;
+
+            mimeMessage.Body=bodybuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("mytraversalproject@gmail.com", "ujwf krbw srhn hqgd");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ TraversalPresentationLayer/Models/MailRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace TraversalPresentationLayer.Models
+{
+    public class MailRequest
+    {
+
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
